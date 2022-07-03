c8fafa79883fe095517dcf1209852e63c58afa20
samirajani18 <103405842+samirajani18@users.noreply.github.com>
2022-07-04 04:35:00 +0530

    Update user_setup.sh

--- user_setup.sh
+++ user_setup.sh
@@ -107,7 +107,7 @@ update_conf()
 
 USER="devops"
 GROUP="devops"
-passw="today@1234"
+passw="rcoem@cse"
 
 if [ -f /etc/os-release ];then
    osname=`grep ID /etc/os-release | egrep -v 'VERSION|LIKE|VARIANT' | cut -d'=' -f2 | sed -e 's/"//' -e 's/"//'`
